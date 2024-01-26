058baa41e61ca9351c52c4223049eb60f1538713
Nagu1244 <shabad.nagaraj@gmail.com>
2024-01-26 21:27:52 +0530

    push

--- src/arraysprograms/PivotalIndex.java
+++ src/arraysprograms/PivotalIndex.java
@@ -1,25 +1,26 @@
 package arraysprograms;
 
+import java.util.Arrays;
+
 public class PivotalIndex {
     public static void main(String args[]){
-        int arr[] = new int[]{1, 7, 3, 6, 5, 6};
-        int pivotIndex= findPivotIndex(arr);
+        int arr[] = new int[]{1, 7, 3, 6, 5, 6};//28
+        //int arr[] = new int[]{1,0,1};
+        int pivotIndex = findPivotIndex(arr);
         System.out.println(pivotIndex);
     }
 
     private static int findPivotIndex(int[] arr) {
-        int leftSum =0;
-        int rightSum=0;
-        for (int i:arr) {
-         rightSum = rightSum + arr[i];
-        }
-        for (int i=0;i<arr.length;i++) {
-           leftSum = leftSum + arr[i];
-           if (leftSum == rightSum) {
-               return i;
-           }
-           rightSum = rightSum-arr[i];
+        int leftSum = 0;
+        int rightSum = Arrays.stream(arr).sum();
+        for(int i=0;i<arr.length;i++) {
+            rightSum = rightSum - arr[i];
+            if(leftSum == rightSum) {
+                return i;
+            }
+            leftSum = leftSum + arr[i];
         }
         return -1;
+
     }
 }
